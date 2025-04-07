0dd673b639c074d9b21b06f0ec4cab7ca445ad65
LauM25 <laurammwork93@gmail.com>
2025-04-07 12:27:50 +0200

    media querys

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -4,7 +4,9 @@ function CharacterDetail({ character }) {
     <>
       <div>
         <Link to="/">
-          <p className="back">Back</p>
+          <p className="back">
+            <i className="fa-solid fa-chevron-left"></i> Back
+          </p>
         </Link>
         <div className="cardDetail">
           <img
@@ -13,11 +15,11 @@ function CharacterDetail({ character }) {
             alt={character.character}
           />
           <div className="cardDetail-txt">
-            <h3>Character:{character.character}</h3>
-            <p>Status:{character.status}</p>
-            <p>Species:{character.species}</p>
-            <p>Origin:{character.origin}</p>
-            <p>Episodes:{character.episodes}</p>
+            <h3>Character: {character.character}</h3>
+            <p>Status: {character.status}</p>
+            <p>Species: {character.species}</p>
+            <p>Origin: {character.origin}</p>
+            <p>Episodes: {character.episodes}</p>
           </div>
         </div>
       </div>
